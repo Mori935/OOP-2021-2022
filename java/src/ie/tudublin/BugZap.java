73a3e1252f0381a9fedd16d30e58cea508fdeecf
Mori935 <C20432034@mytudublin.ie>
2022-02-07 11:22:58 +0000

    BugZap

---
+++ java/src/ie/tudublin/BugZap.java
@@ -0,0 +1,31 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class BugZap extends PApplet 
+{
+    public void settings()
+	{
+		size(500,500);
+	}
+
+    float playerX = 0;
+    float playerY = 20;
+    float playerWidth = 35;
+
+    public void setup() 
+    {
+
+	}
+
+    public void draw()
+	{
+
+	}
+
+    public void drawPlayer(float x, float y, float w)
+    {
+        
+    }
+    
+}
